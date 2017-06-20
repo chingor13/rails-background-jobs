5c1d87859ac5d7207717d8b10d6e0f85b5a1b393
Jeff Ching <chingor@google.com>
2017-06-20 10:04:31 -0700

    Remove google-cloud-debugger

--- config/application.rb
+++ config/application.rb
@@ -15,12 +15,5 @@ module RailsBackgroundJobs
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :google_cloud_pubsub
-    # config.active_job.queue_adapter = ActiveJob::GoogleCloudPubsub::Adapter.new(
-    #   pubsub: Google::Cloud::Pubsub.new(
-    #     project: 'chingor-rails'
-    #   )
-    # )
-    # config.google_cloud.use_logging = true
-    # config.google_cloud.use_debugger = true
   end
 end
